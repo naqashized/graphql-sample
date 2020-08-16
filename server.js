f1f64f7c7f43c7777f6e1e7e822dcbae59760fa8
tahirsafari <tahir.naqash@safarifoneict.com>
2020-08-16 11:32:56 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Initialize a GraphQL schema
+var schema = buildSchema(`
+  type Query {
+    user(id: Int!): User
+    users(name: String): [User]
+  },
+  type User {
+    id: Int
+    name: String
+    age: Int
+    address: String
+    dob: String
+  }
+`);
+
+// Sample users
+var users = [
+  {
+    id: 1,
+    name: 'David',
+    age: '21',
+    address: 'Street 21, Block 1',
+    dob: '12/09/1981'
+  },
+  {
+    id: 2,
+    name: 'Harley',
+    age: '22',
+    address: 'Street 21, Block 2',
+    dob: '12/09/1909'
+  },
+  {
+    id: 3,
+    name: 'Bob',
+    age: '23',
+    address: 'Street 21, Block 3',
+    dob: '12/09/1994'
+  },
+  {
+    id: 4,
+    name: 'Josephine',
+    age: '23',
+    address: 'Street 21, Block 4',
+    dob: '12/09/1989'
+  }
+];
+
+// Return a single user (based on id)
+var getUser = function(args) {
+  var userID = args.id;
+  return users.filter(user => user.id == userID)[0];
+}
+
+// Return a list of users (takes an optional name parameter)
+var retrieveUsers = function(args) {
+  //if (args.name) {
+  //   var name = args.name;
+  //   return users.filter(user => user.name === name);
+  // } else {
+    return users;
+  //}
+}
+
+// Root resolver
+var root = { 
+  user: getUser,  // Resolver function to return user with specific id
+  users: retrieveUsers
+};
+
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,  // Must be provided
+  rootValue: root,
+  graphiql: true,  // Enable GraphiQL when server endpoint is accessed in browser
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
